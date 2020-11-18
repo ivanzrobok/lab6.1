4053743fc03b93b330042d661e6e7f5faa6e3115
ivanzrobok <ivan.zrobok.itis.2020@lpnu.ua>
2020-11-18 11:04:20 +0200

    test lab6.1.1

---
+++ lab6.1.1Test/lab6.1.1Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab611Test
+{
+	TEST_CLASS(lab611Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
